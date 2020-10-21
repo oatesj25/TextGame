6471919e49a87619a1450ebb9d659dc810af48ca
Eric Fricke <45500319+frickeet@users.noreply.github.com>
2020-10-20 20:55:43 -0500

    Got rid of unnecessary lines

--- COMP SCI 316 - Midterm Project/src/GameDriver.java
+++ COMP SCI 316 - Midterm Project/src/GameDriver.java
@@ -1,39 +1,24 @@
 
-import javafx.fxml.FXMLLoader;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class GameDriver extends Application {
+   @Override
+   public void start(Stage stage) throws Exception {
+      Parent root = 
+         FXMLLoader.load(getClass().getResource("textGame.fxml"));
 
-	private Stage primaryStage;
-	
-	@Override
-	public void start(Stage primaryStage) {
-		this.primaryStage = primaryStage;
-		mainWindow();
-	}
-	
-	public void mainWindow() {
-		try {
-			System.out.println("GameDriver method mainWindow called");
-			FXMLLoader loader = new FXMLLoader(GameDriver.class.getResource("textGame.FXML"));
-			Pane pane = loader.load();
-			
-			GameController gameController = loader.getController();
-			gameController.setGameDriver(this);
-			
-			Scene scene = new Scene(pane);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		}
-			catch (Exception e) {
-				e.printStackTrace();
-			}
-	}
-	
-	public static void main(String[] args) throws InterruptedException {
-		launch(args);
-	}
+      Scene scene = new Scene(root); 
+      stage.setTitle("Saharan Delirium"); 
+      stage.setScene(scene);
+      stage.show();
+   }
+
+   public static void main(String[] args) {
+      launch(args); 
+   }
 }
+
